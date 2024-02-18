d2ce0a492c3dfd612b62121291fefdb0a84d7035
NachiketSaraf <sarafnachiket@gmail.com>
2024-02-19 01:09:47 +0530

    .....

---
+++ src/test/java/com/spring/boot/JPACRUD/JpaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.JPACRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
