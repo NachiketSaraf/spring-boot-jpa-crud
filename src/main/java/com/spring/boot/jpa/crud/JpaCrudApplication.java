48be2a77cecdf421d3f0c256784956a12887a88b
NachiketSaraf <sarafnachiket@gmail.com>
2024-03-20 23:09:39 +0530

    .....

--- src/main/java/com/spring/boot/jpa/crud/JpaCrudApplication.java
+++ src/main/java/com/spring/boot/jpa/crud/JpaCrudApplication.java
@@ -1,13 +1,17 @@
 package com.spring.boot.jpa.crud;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Tours JPA",
+        description = "Spring Boot CRUD using JPA " ))
 public class JpaCrudApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JpaCrudApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JpaCrudApplication.class, args);
+    }
 
 }
